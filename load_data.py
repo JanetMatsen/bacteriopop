7471bbef13035b1815232861b14dd757c196d1b1
Pearl Philip <pearlphilip007@gmail.com>
2016-02-23 18:41:16 -0800

    Made column names small-caps, solved PEP8 compliance issues

--- load_data.py
+++ load_data.py
@@ -2,10 +2,17 @@ import pandas as pd
 
 
 def load_data():
+
+    """Reads two dataframes from Github source, containing sample data
+     and meta data and joins them.
+     Returns one dataframe with a sampleID index"""
+
     # Reading data sets from the links provided.
-    df1 = pd.read_csv("https://raw.githubusercontent.com/JanetMatsen/bacteriopop/master/raw_data/raw_data.csv",
+    df1 = pd.read_csv("https://raw.githubusercontent.com/JanetMatsen/"
+                      "bacteriopop/master/raw_data/raw_data.csv",
                       error_bad_lines=False)
-    df2 = pd.read_csv('https://raw.githubusercontent.com/JanetMatsen/bacteriopop/master/raw_data/sample_meta_info.tsv',
+    df2 = pd.read_csv('https://raw.githubusercontent.com/JanetMatsen/'
+                      'bacteriopop/master/raw_data/sample_meta_info.tsv',
                       sep='\t')
     df2 = df2.set_index(df2['project'])
     # Removing duplicate columns.
@@ -16,18 +23,20 @@ def load_data():
     del df1['project']
     # Joining the two datasets.
     df = df1.join(df2)
-    # Uniformity in capitalization of column names.
-    df.rename(columns={'abundance': 'Abundance', 'oxygen': 'Oxygen', 'replicate': 'Replicate',
-                       'week': 'Week'}, inplace=True)
-    df.index.names = ['SampleID']
+    # Uniformity in non-capitalization of column names.
+    df.rename(columns={'Kingdom': 'kingdom', 'Phylum': 'phylum',
+                       'Class': 'class', 'Order': 'order',
+                       'Family': 'family', 'Genus': 'genus',
+                       'Length': 'length'}, inplace=True)
+    df.index.names = ['sampleID']
     # Rearranging columns so that abundance is the last column.
-    df = df[['Kingdom',	'Phylum',	'Class', 'Order',	'Family',	'Genus',	'Length',	'Oxygen',	'Replicate',
-             'Week', 'Abundance']]
+    df = df[['kingdom',	'phylum',	'class', 'order',
+             'family',	'genus',	'length',	'oxygen',
+             'replicate', 'week', 'abundance']]
     return df
 
+
 # Run the following code if the file is run at the command line
-
-
 def main():
     return load_data()
 
