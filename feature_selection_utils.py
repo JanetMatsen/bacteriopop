95ee498ab3cd9f819f246f859431a4ef687c520a
Saghar Hosseini <saghar@uw.edu>
2016-02-24 17:47:05 -0800

    the long lines (>79) are shortened

--- feature_selection_utils.py
+++ feature_selection_utils.py
@@ -6,10 +6,10 @@ from sklearn.decomposition import PCA
 
 def calculate_features_target_correlation(data, features, target, method):
     """
-    This function receives the pandas dataframe, the list of features, and a string that representing the name of the
-    target column in data frame.
-    The output is the correlation of each numeric feature vector with the target values and their bar plot.
-    method specifies the type of correlation method.
+    This function receives the pandas dataframe, the list of features, and
+    a string that representing the name of the target column in data frame.
+    The output is the correlation of each numeric feature vector with the
+    target values and their bar plot.
     :type data: pandas dataframe
     :param data: data set to be analysed
     :type features: list of strings
@@ -17,7 +17,8 @@ def calculate_features_target_correlation(data, features, target, method):
     :type target: string
     :param target: the column of data set that is treated as the output for prediction
     :type method: string
-    :param method: specisifes the method for finding the correlation coefficients and can be "Pearson" or "Cross-cor"
+    :param method: specisifes the method for finding the correlation coefficients and
+                   can be "Pearson" or "Cross-cor"
     :return cor: a list of correlation coefficients between features and target values
     """
     # target values are assigned to y
@@ -47,8 +48,8 @@ def calculate_features_target_correlation(data, features, target, method):
 
 def pca_bacteria(data, n_components):
     """
-    calculate Principal Components Analysis from data in a pandas dataframe and n_components represents the desire
-    number of principal components
+    calculate Principal Components Analysis from data in a pandas dataframe
+    and n_components represents the desire number of principal components
     :type data: pandas dataframe
     :param data: data set to be analysed
     :type n_components: int
