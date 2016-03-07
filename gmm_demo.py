b68cff55bfa9748de3f3463c358d2a6888565f2d
Pearl Philip <pearlphilip007@gmail.com>
2016-03-07 13:52:27 -0800

    Added rudimentary printable tracking strings

--- gmm_demo.py
+++ gmm_demo.py
@@ -2,13 +2,20 @@ from gmm import gmm
 from load_data import load_data
 from bacteriopop_utils import extract_features
 
+
 def gmm_demo():
+    print 'starting up'
     df = load_data()
+    print 'load done'
     df = extract_features(df)
+    print 'extract done'
     features_list = list(df.columns.values)[1:]
+    print 'features done'
     gmm(df, features_list)
 
 # Run the following code if the file is run at the command line
+
+
 def main():
     return gmm_demo()
 
