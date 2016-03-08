3ca0589242a8df5faaaa57bfbde5942a55ede3c1
Saghar Hosseini <saghar@uw.edu>
2016-03-08 12:05:56 -0800

    added time-verying adjacency matrices, the dictionary keys are in the format: ex. ('High',1,'Week 1') for the temporal adjacency

--- dynamic_mode_decomposition.py
+++ dynamic_mode_decomposition.py
@@ -5,7 +5,11 @@ from sklearn.preprocessing import normalize
 from bacteriopop_utils import prepare_DMD_matrices
 
 
-def find_adjecency_matrix(min_abundance, phylo_column, full_svd):
+def find_fixed_adjacency_matrix(min_abundance, phylo_column, full_svd):
+    '''
+    This function find the adjacency matrix among clusters of bacteria over
+    the 11 weeks of sampling assuming the interaction between clusters is fixed.
+    '''
     # Default values
     if min_abundance is None:
         min_abundance = 0
@@ -21,7 +25,7 @@ def find_adjecency_matrix(min_abundance, phylo_column, full_svd):
         data = df.values
         X = data[:, 0:10]
         Y = data[:, 1:11]
-        # Preprocess the abudance data
+        # Preprocess the abundance data
         X = normalize(X, axis=0)
         Y = normalize(Y, axis=0)
         U, s, V = np.linalg.svd(X, full_matrices=full_svd)
@@ -37,3 +41,42 @@ def find_adjecency_matrix(min_abundance, phylo_column, full_svd):
         linear_mappings[descriptive_tuple] = A
     return linear_mappings
 
+def find_temporal_adjacency_matrix(min_abundance, phylo_column, full_svd):
+    '''
+    This function find the adjacency matrix among clusters of bacteria from
+    week to week assuming the interaction between clusters is changing.
+    '''
+    # Default values
+    if min_abundance is None:
+        min_abundance = 0
+    if phylo_column is None:
+        phylo_column = 'family'
+    if full_svd is None:
+        full_svd = False
+    # snapshots of samples over 11 weeks
+    snapshots = prepare_DMD_matrices(min_abundance, phylo_column, oxygen='all')
+    linear_mappings = {}
+    for descriptive_tuple in snapshots.keys():
+        df = snapshots[descriptive_tuple]
+        data = df.values
+        for time in range(10):
+            X = data[:, time:time+1]
+            Y = data[:, time+1:time+2]
+            # Preprocess the abundance data
+            X = normalize(X, axis=0)
+            Y = normalize(Y, axis=0)
+            U, s, V = np.linalg.svd(X, full_matrices=full_svd)
+            if full_svd is True:  # slower
+                S = np.zeros((len(U), len(s)), dtype=complex)
+                S[:len(s), :len(s)] = np.diag(s)
+            else:  # faster
+                S = np.diag(s)
+            pseu_inv_x = np.dot(np.linalg.inv(V), np.dot(np.linalg.inv(S), np.linalg.pinv(U)))
+            # Adjacency matrix between clusters
+            A = np.dot(Y, pseu_inv_x)
+            # A = np.dot(Y, np.linalg.pinv(X))  # full SVD (slower)
+            key= descriptive_tuple + ('Week '+ str(time+1),)
+            linear_mappings[key] = A
+    return linear_mappings
+
+find_temporal_adjacency_matrix(0, None, False)
