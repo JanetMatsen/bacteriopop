5339a6c11b657d4045f8d9c0b25ea73679967d06
Saghar Hosseini <saghar@uw.edu>
2016-03-09 20:21:47 -0800

    updated the demo notebook, added description to dmd function, added heatmap and reduced adjacency to the network construction files

--- network_construction.py
+++ network_construction.py
@@ -1,29 +1,49 @@
 import networkx as nx
-import dynamic_mode_decomposition as dmd
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from matplotlib import pylab
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
+from bokeh.sampledata.les_mis import data
+import dynamic_mode_decomposition as dmd
 
 
-def create_one_graph(Adj, nodes, edge_treshhold):
+def create_one_graph_using_networkx(adj, nodes, edge_treshhold):
+    """
+    This function create a graph using the adjacency matrix adj and list of nodes.
+    adj: a numpy array representing the adjacency matrix
+    nodes: a list representing the nodes in the graph
+    edge_treshhold: the treshhold on the elements of adjacency matrix adj for that 
+                    pair to be consider as an edge
+    return: the graph in networkX format
+    """
     # create and empty graph
     G = nx.Graph()
     # add edges to the graph
     N = len(nodes)
     for node1 in range(N):
         for node2 in range(N):
-            if node1 == node2 or Adj[node1][node2] >= edge_treshhold:
+            if node1 == node2 or abs(adj[node1][node2]) >= edge_treshhold:
                 G.add_edge(node1, node2)
-                G[node1][node2]['weight'] = Adj[node1][node2]
+                G[node1][node2]['weight'] = adj[node1][node2]
     return G
 
 
 def create_all_graphs(mappings, nodes_list, edge_treshhold=1e-10):
+    '''
+    This function trnasforms the adjacency matrix into the graph for all the instances
+    '''
     graphs = {}
     for key in mappings.keys():
-        graphs[key] = create_one_graph(mappings[key], nodes_list[key], edge_treshhold)
+        graphs[key] = create_one_graph_using_networkx(mappings[key], nodes_list[key], edge_treshhold)
     return graphs
 
+
 def save_graph(graph,file_name):
+    """
+    This function save the graph into a file called file_name
+    """
     #initialze Figure
     plt.figure(num=None, figsize=(20, 20), dpi=80)
     plt.axis('off')
@@ -43,11 +63,70 @@ def save_graph(graph,file_name):
     pylab.close()
     del fig
 
-# #Assuming that the graph g has nodes and edges entered
-# mappings, nodes_list = dmd.find_fixed_adjacency_matrix(0,None,False)
+
+def reduce_adjacency_matrix(Adj,nodes,edge_treshhold):
+    '''
+    This function removes some rows and columns of the adjacency matrix
+    according to their correlation with other elements in the adjacency
+    matrix and returns a new (smaller) numpy array for adjacency and the
+    new list of nodes considered in the matrix.
+    '''
+    N=len(nodes)
+    new_nodes_index=[]
+    for node1 in range(N):
+        remove=True
+        for node2 in range(N):
+            if abs(Adj[node1][node2])>edge_treshhold or abs(Adj[node2][node1])>edge_treshhold:
+                remove=False
+        if not remove:
+            new_nodes_index.append(node1)
+    new_adj=np.zeros([len(new_nodes_index),len(new_nodes_index)])
+    for i,node1 in enumerate(new_nodes_index):
+        for j,node2 in enumerate(new_nodes_index):
+            new_adj[i][j]=Adj[node1][node2]
+    return new_adj, new_nodes_index
+
+
+def generate_x_y(adj):
+    '''
+    This function creates a meshgrid for the adjacecny matrix in x-y plane
+    '''
+    s = adj.shape
+    x, y = np.meshgrid(np.arange(s[0]), np.arange(s[1]))
+    return x.ravel(), y.ravel()
+
+
+def heatmap(adj,nodes):
+    '''
+    This function creates a heat map for the numpy array adj
+    '''
+    x, y = generate_x_y(adj)
+    x_min=0
+    y_min=0
+    x_max=len(nodes)-1
+    y_max=len(nodes)-1
+    plt.figure()
+    plt.hexbin(x, y, C=adj.ravel(),gridsize=len(nodes)/2,cmap=cm.jet,bins=None,
+               mincnt=-100,extent=[x_min, x_max, y_min, y_max])
+
+def adjacency_matrix_heatmap(Adj,nodes,figure_title,file_name):
+    '''
+    This function creates heat maps for the adjacency matrix Adj
+    and save it into the file_name
+    The Adj is in numpy array format
+    nodes is the list of nodes
+    figure_title is a string represnting the tile of the saved figure
+    '''
+    heatmap(Adj,nodes)
+    cb = plt.colorbar()
+    cb.set_label('abundance')
+    plt.title(figure_title)
+    plt.savefig(file_name,bbox_inches="tight")
+    # plt.show()
+
+
+# mappings, nodes_list = dmd.find_fixed_adjacency_matrix(0,'order',False)
 # for key in mappings.keys():
-#     Adj = mappings[key]
-#     nodes = nodes_list[key]
-#     g = create_one_graph(Adj,nodes,edge_treshhold=1e-10)
-    # file_name="graph.pdf"
-    # save_graph(g,file_name)
+#     Adj,nodes=reduce_adjacency_matrix(mappings[key],nodes_list[key],edge_treshhold=0.5)
+#     file_name=str(key[0])+str(key[1])+'.pdf'
+#     adjacency_matrix_heatmap(Adj,nodes,str(key),file_name)
