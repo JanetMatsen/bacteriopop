94d42f56d0d3d98cbedb6679bafbe6fb890cca40
Pearl Philip <pearlphilip007@gmail.com>
2016-03-11 07:58:04 -0800

    Added demo

--- dbscan.py
+++ dbscan.py
@@ -3,7 +3,11 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import DBSCAN
+from load_data import load_data
+from bacteriopop_utils import extract_features
 
+DF = load_data()
+DF = extract_features(DF)
 
 def get_random_color(pastel_factor=0.5):
     return [(x+pastel_factor)/(1.0+pastel_factor) for x in
@@ -21,17 +25,18 @@ def generate_new_color(existing_colors, pastel_factor=0.5):
         color = get_random_color(pastel_factor=pastel_factor)
         if not existing_colors:
             return color
-        best_distance = min([color_distance(color, c) for c in existing_colors])
+        best_distance = min([color_distance(color, c) for c in
+                             existing_colors])
         if not max_distance or best_distance > max_distance:
             max_distance = best_distance
             best_color = color
     return best_color
 
 
-def dbscan(dataframe, eps=0.2, min_samples=10):
+def dbscan(DF, eps=0.5, min_samples=3):
 
     # Compute DBSCAN.
-    db = DBSCAN(eps, min_samples).fit(dataframe)
+    db = DBSCAN(eps, min_samples).fit(DF)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
     labels = db.labels_
@@ -40,18 +45,6 @@ def dbscan(dataframe, eps=0.2, min_samples=10):
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
     print('Estimated number of clusters: %d' % n_clusters_)
-    """ print('Homogeneity: %0.3f'
-          % metrics.homogeneity_score(labels_true, labels))
-    print('Completeness: %0.3f'
-          % metrics.completeness_score(labels_true, labels))
-    print('V-measure: %0.3f'
-          % metrics.v_measure_score(labels_true, labels))
-    print('Adjusted Rand Index: %0.3f'
-          % metrics.adjusted_rand_score(labels_true, labels))
-    print('Adjusted Mutual Information: %0.3f'
-          % metrics.adjusted_mutual_info_score(labels_true, labels))
-    print('Silhouette Coefficient: %0.3f'
-          % metrics.silhouette_score(df, labels))"""
 
     # Plot result.
     # Black removed and is used for noise instead.
@@ -66,12 +59,30 @@ def dbscan(dataframe, eps=0.2, min_samples=10):
             col = 'k'
 
         class_member_mask = (labels == k)
-        xy = dataframe[class_member_mask & core_samples_mask]
+        xy = DF[class_member_mask & core_samples_mask]
         plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
                  markeredgecolor='k', markersize=14)
-        xy = dataframe[class_member_mask & ~core_samples_mask]
+        xy = DF[class_member_mask & ~core_samples_mask]
         plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
                  markeredgecolor='k', markersize=6)
 
     plt.title('Estimated number of clusters %d' % n_clusters_)
     plt.show()
+
+
+def dbscan_demo():
+    print 'starting up'
+    df = load_data()
+    print 'load done'
+    df = extract_features(df)
+    print 'features done'
+    dbscan(df, 0.2, 10)
+
+# Run the following code if the file is run at the command line
+
+
+def main():
+    return dbscan_demo()
+
+if __name__ == "__main__":
+    main()
